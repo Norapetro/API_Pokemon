46df420546ce8947e76bfdb672b0aef2b738c08f
Jhon Erik <javilaortiz@gmail.com>
2023-12-19 20:50:06 -0500

    implementation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/Header.module.scss";
 import pokeball from "../assets/pokeball.svg";
+import { Button, ButtonGroup, Input } from 'reactstrap';
 import PropTypes from "prop-types";
 
 const Header = ({ onSearchChange }) => {
@@ -10,17 +11,21 @@ const Header = ({ onSearchChange }) => {
         {/* Logo o título de la aplicación */}
         <Link to="/"><img width={30} src={pokeball} alt="pokeball" /></Link>
       </div>
-      <input
+      <Input
         type="text"
         className={styles.searchInput}
         placeholder="Buscar Pokémon..."
         onChange={(event) => onSearchChange(event.target.value)}
       />
-      <nav className={styles.navigation}>
-        {/* Links de navegación */}
-        <Link to="/" onClick={() => onSearchChange("")}>Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      {/* Links de navegación */}
+      <ButtonGroup>
+        <Button color="primary">
+          <Link className={styles.link} to="/" onClick={() => onSearchChange("")}>Home</Link>
+        </Button>
+        <Button color="primary">
+          <Link to="/about" className={styles.link}>About</Link>
+        </Button>
+      </ButtonGroup>
     </header>
   );
 };
