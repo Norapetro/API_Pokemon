33b2bebba8bf22ba20495c6a1fce77f664745b75
Jhon Erik <javilaortiz@gmail.com>
2023-12-20 19:59:28 -0500

    axios implementation

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,24 @@
+import axios from "axios";
+
 export const getPokemons = async () => {
+  try {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100");
+    const { results } = response.data;
+    const pokemons = await Promise.all(
+      results.map(async ({ url }) => {
+        const response = await axios.get(url);
+        return response.data;
+      })
+    );
+    return pokemons;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
+/* export const getPokemons = async () => {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
   const { results } = await response.json();
   // fetch all pokemons
@@ -9,4 +29,6 @@ export const getPokemons = async () => {
     })
   );
   return pokemons;
-};
+}; */
+
+
